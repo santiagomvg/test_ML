891e08aad54c6f4d634cb80834504fb5418e8acc
santiagomvg <santiago.m.vidal.g@gmail.com>
2020-04-27 19:39:39 -0300

    stats almost ready

--- stats.go
+++ stats.go
@@ -12,13 +12,13 @@ const countriesRequestsSetKey = "stats:requests:countries" //redis set of all co
 const countryRequestCount = "stats:request:country:%s"     //request count for country %$
 
 type countryStat struct {
-	Name     string  `json:"name"`
+	Country  string  `json:"country"`
 	Distance float64 `json:"distance"`
 }
 
 func handleStatsNearest(c *gin.Context) {
 
-	stat, err := getCountryDistance(nearestCountryKey, "ARG:BA")
+	stat, err := getStoredCountryDistance(nearestCountryKey, "ARG:BA")
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
@@ -27,7 +27,7 @@ func handleStatsNearest(c *gin.Context) {
 }
 
 func handleStatsFarthest(c *gin.Context) {
-	stat, err := getCountryDistance(farthestCountryKey, "ARG:BA")
+	stat, err := getStoredCountryDistance(farthestCountryKey, "ARG:BA")
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
@@ -38,16 +38,20 @@ func handleStatsFarthest(c *gin.Context) {
 func handleStatsAVG(c *gin.Context) {
 	s := DB.Session()
 	defer s.Close()
+
+	//TODO:
+	//sort stats:requests:countries by nosort get # get stats:request:country:*
 }
 
-func getCountryDistance(key string, from string) (*countryStat, error) {
+func getStoredCountryDistance(key string, from string) (*countryStat, error) {
+
+	var stat countryStat
+	key = fmt.Sprintf(key, from)
 
 	s := DB.Session()
 	defer s.Close()
 
-	var stat countryStat
-	key = fmt.Sprintf(key, from)
-	if err := s.HGET(key, "country,distance", &stat); err != nil {
+	if err := s.HGETALL(key, &stat); err != nil {
 		return nil, err
 	}
 	return &stat, nil
@@ -56,25 +60,29 @@ func getCountryDistance(key string, from string) (*countryStat, error) {
 func updateStatsForCountry(distance float64, fromGeoLocationCode string, cinfo *CountryInfo) {
 
 	//TODO: meter todo en go funcs individuales?
-	stat, err := getCountryDistance(nearestCountryKey, fromGeoLocationCode)
+	stat, err := getStoredCountryDistance(nearestCountryKey, fromGeoLocationCode)
 	if err == nil {
 		if distance < stat.Distance {
-			stat.Name = cinfo.Name
+			stat.Country = cinfo.Name
 			stat.Distance = distance
 			s := DB.Session()
 			defer s.Close()
-			s.HSET(nearestCountryKey, "country,distance", &stat)
+
+			key := fmt.Sprintf(nearestCountryKey, fromGeoLocationCode)
+			s.HMSET(key, stat)
 		}
 	}
 
-	stat, err = getCountryDistance(farthestCountryKey, fromGeoLocationCode)
+	stat, err = getStoredCountryDistance(farthestCountryKey, fromGeoLocationCode)
 	if err == nil {
 		if distance > stat.Distance {
-			stat.Name = cinfo.Name
+			stat.Country = cinfo.Name
 			stat.Distance = distance
 			s := DB.Session()
 			defer s.Close()
-			s.HSET(farthestCountryKey, "country,distance", &stat)
+
+			key := fmt.Sprintf(farthestCountryKey, fromGeoLocationCode)
+			s.HMSET(key, stat)
 		}
 	}
 
